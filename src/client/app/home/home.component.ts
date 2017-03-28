4035435d0beaeb10dc601088e48ba29f0c0242c9
Sergey Pudov <pudov_sa@bw-sw.com>
2017-03-28 16:27:20 +0700

    SOP-22 CoreUI Theme basic integration

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,56 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { NameListService } from '../shared/name-list/name-list.service';
+import { Component } from '@angular/core';
+import { Config } from '../shared/config/env.config';
 
 /**
  * This class represents the lazy loaded HomeComponent.
  */
 @Component({
   moduleId: module.id,
-  selector: 'sd-home',
-  templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css'],
+  selector: 'bs-home',
+  templateUrl: 'home.component.html'
 })
-export class HomeComponent implements OnInit {
-
-  newName: string = '';
-  errorMessage: string;
-  names: any[] = [];
-
-  /**
-   * Creates an instance of the HomeComponent with the injected
-   * NameListService.
-   *
-   * @param {NameListService} nameListService - The injected NameListService.
-   */
-  constructor(public nameListService: NameListService) {}
-
-  /**
-   * Get the names OnInit
-   */
-  ngOnInit() {
-    this.getNames();
+export class HomeComponent {
+  constructor() {
+    console.log(Config);
   }
-
-  /**
-   * Handle the nameListService observable
-   */
-  getNames() {
-    this.nameListService.get()
-      .subscribe(
-        names => this.names = names,
-        error => this.errorMessage = <any>error
-      );
-  }
-
-  /**
-   * Pushes a new name onto the names array
-   * @return {boolean} false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    // TODO: implement nameListService.post
-    this.names.push(this.newName);
-    this.newName = '';
-    return false;
-  }
-
 }
