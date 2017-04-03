e11aa0e38ccc16bbbd800f847a59068d78109069
Darya Baklanova <darya.baklanova@gmail.com>
2017-04-03 16:50:45 +0700

    SOP-27 View of language selector changed

---
+++ src/client/app/core/components/language/languages.ts
@@ -0,0 +1,6 @@
+import { SupportedLanguages } from '../../../shared/config/translate-loader.config';
+
+export const LANGUAGES = {
+  [SupportedLanguages.EN]: 'T_LANG_EN',
+  [SupportedLanguages.RU]: 'T_LANG_RU'
+};
