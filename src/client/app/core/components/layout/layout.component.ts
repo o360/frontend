7ead9b0344dbe65a4d09b2060a665c5ba5b0e716
Sergey Pudov <pudov_sa@bw-sw.com>
2017-04-11 08:35:01 +0700

    SOP-31 Separated login and client pages

---
+++ src/client/app/core/components/layout/layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'bs-layout',
+  templateUrl: 'layout.component.html'
+})
+export class LayoutComponent {
+}
