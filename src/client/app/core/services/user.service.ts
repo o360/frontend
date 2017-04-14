9eb886f51da8348379350c39b2e88a1e5aa8cf82
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-14 10:33:23 +0700

    SOP-41 Integrate with API SOP-39 (User's data changing)

--- src/client/app/core/services/user.service.ts
+++ src/client/app/core/services/user.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@angular/core';
 import { RestServiceConfig } from '../decorators/rest-service-config.decorator';
 import { UserModel } from '../models/user-model';
 import { FirebaseRestService } from './firebase-rest.service';
+import { RestService } from './rest.service';
 
 @Injectable()
 @RestServiceConfig({
   entityName: 'users',
   entityConstructor: UserModel
 })
-export class UserService extends FirebaseRestService<UserModel> {
+export class UserService extends RestService<UserModel> {
 }
--- src/client/app/user-profile/user-profile.component.ts
+++ src/client/app/user-profile/user-profile.component.ts
@@ -1,21 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Model, ModelId } from '../core/models/model';
+import { UserService } from '../core/services/user.service';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   moduleId: module.id,
   selector: 'bs-user-profile',
-  templateUrl: 'user-profile.component.html'
+  templateUrl: 'user-profile.component.html',
 })
-export class UserProfileComponent<UserModel> {
-  protected _fakeUser = {
-    name: 'Harry Potter',
-    email: 'just-harry@gm.com'
-  };
+export class UserProfileComponent<UserModel extends Model> implements OnInit {
+  protected _user: UserModel;
+  protected _id: ModelId;
 
-  public get fakeUser(): Object {
-    return this._fakeUser;
+  public get user(): UserModel {
+    return this._user;
   }
 
-  public closeProfile() {
-    console.log('close');
+  constructor(protected _service: UserService,
+              protected _auth: AuthService) {
+  }
+
+  public ngOnInit() {
+    this._id = this._auth.user.id;
+    this._update();
+  }
+
+  protected _update() {
+    this._service.get(this._id).subscribe((user: UserModel) => {
+      this._user = user;
+    });
   }
 }
