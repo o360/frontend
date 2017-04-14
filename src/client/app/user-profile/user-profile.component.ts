83246504f14ab08f2cda501dc8834cc2156144ae
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-14 13:46:15 +0700

    SOP-41 Fixes after review

--- src/client/app/user-profile/user-profile.component.ts
+++ src/client/app/user-profile/user-profile.component.ts
@@ -2,32 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Model, ModelId } from '../core/models/model';
 import { UserService } from '../core/services/user.service';
 import { AuthService } from '../core/services/auth.service';
+import { DetailsComponent } from '../shared/components/details.component';
+import { UserModel } from '../core/models/user-model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'bs-user-profile',
   templateUrl: 'user-profile.component.html',
 })
-export class UserProfileComponent<UserModel extends Model> implements OnInit {
-  protected _user: UserModel;
-  protected _id: ModelId;
-
-  public get user(): UserModel {
-    return this._user;
-  }
-
-  constructor(protected _service: UserService,
+export class UserProfileComponent extends DetailsComponent<UserModel> implements OnInit {
+  constructor(service: UserService,
+              route: ActivatedRoute,
               protected _auth: AuthService) {
+    super(service, route);
   }
 
   public ngOnInit() {
     this._id = this._auth.user.id;
     this._update();
-  }
-
-  protected _update() {
-    this._service.get(this._id).subscribe((user: UserModel) => {
-      this._user = user;
-    });
   }
 }
