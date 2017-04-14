5b23cf706f317a62361162a8dc18539ffa471834
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-14 10:33:23 +0700

    SOP-41 Integrate with API SOP-39 (User's data changing)

--- src/client/app/user-profile/user-profile-edit.component.ts
+++ src/client/app/user-profile/user-profile-edit.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
+import { UserProfileComponent } from './user-profile.component';
+import { UserModel } from '../core/models/user-model';
+import { UserService } from '../core/services/user.service';
+import { AuthService } from '../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'bs-user-profile-edit',
   templateUrl: 'user-profile-edit.component.html'
 })
-export class UserProfileEditComponent {
-  protected _fakeUser = {
-    name: 'Harry Potter',
-    email: 'just-harry@gm.com'
-  };
+export class UserProfileEditComponent extends UserProfileComponent<UserModel> {
+  protected _returnPath = ['/profile'];
 
-  public get fakeUser(): Object {
-    return this._fakeUser;
+  constructor(service: UserService,
+              auth: AuthService,
+              protected _router: Router) {
+    super(service, auth);
   }
 
   public submitProfileChanges() {
-    console.log('submitProfileChanges');
+    this._service.save(this._user).subscribe(() => {
+      if (this._returnPath) {
+        this._router.navigate(this._returnPath);
+      }
+    });
   }
 }
