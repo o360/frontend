6a608a5bd7297cb77c8e76ae3d441969fb25b064
Darya Baklanova <darya.baklanova@gmail.com>
2017-04-17 15:57:51 +0700

    SOP-51 After rebase

--- src/client/app/admin/admin.module.ts
+++ src/client/app/admin/admin.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
-import { UserFormComponent } from './user/user-edit/user-form.component';
 
 @NgModule({
   imports: [
     AdminRoutingModule,
     SharedModule
   ],
-  declarations: [
-    UserFormComponent
-  ],
-  exports: [
-    UserFormComponent
-  ]
+  declarations: [],
+  exports: []
 })
 export class AdminModule {
 }
