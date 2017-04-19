5a8eb0fc5e416135ed1c5282d1e910a99b432986
Darya Baklanova <darya.baklanova@gmail.com>
2017-04-19 14:45:38 +0700

    SOP-57 After Code Review #2

--- src/client/app/shared/directives/confirmation.directive.ts
+++ src/client/app/shared/directives/confirmation.directive.ts
@@ -1,30 +1,37 @@
 import { Directive, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Directive({
   selector: '[bsConfirm]'
 })
 export class ConfirmationDirective {
-  protected _message: string = 'Are you sure?';
+  protected _message: string;
 
   @Input()
-  public set message(value: string) {
+  public set message(value: any) {
     this._message = value;
   }
 
   @Output()
-  public confirm: EventEmitter<void> = new EventEmitter<void>();
+  public _confirm: EventEmitter<void> = new EventEmitter<void>();
 
   @Output()
-  public cancel: EventEmitter<void> = new EventEmitter<void>();
+  public _cancel: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(protected _translateService: TranslateService) {
+    this._translateService.get('T_CONFIRM_MESSAGE').subscribe((res: string) => {
+      this._message = res;
+    });
+  }
 
   @HostListener('click', ['$event'])
   public clickHandler() {
     const confirmed = window.confirm(this._message);
 
     if (confirmed) {
-      this.confirm.emit();
+      this._confirm.emit();
     } else {
-      this.cancel.emit();
+      this._cancel.emit();
     }
   }
 
