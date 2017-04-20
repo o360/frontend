40a2d852ddc5e7b5039bd4168c5d905446a47b92
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-20 16:57:58 +0700

    SOP-59 Orgstructure group creation

--- src/client/app/admin/groups/group-form.component.ts
+++ src/client/app/admin/groups/group-form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormComponent } from '../../shared/components/form.component';
 import { GroupModel } from '../../core/models/group-model';
 import { GroupService } from '../../core/services/group.service';
+import { ModelId } from '../../core/models/model';
 
 
 @Component({
@@ -10,24 +11,48 @@ import { GroupService } from '../../core/services/group.service';
   selector: 'bs-group-form-details',
   templateUrl: 'group-form.component.html'
 })
-export class GroupFormComponent extends FormComponent<GroupModel> {
-  protected _parentsList: GroupModel[];
+export class GroupFormComponent extends FormComponent<GroupModel> implements OnInit {
+  protected _groups: GroupModel[];
+  protected _parentId: ModelId = null;
 
-  public get parentsList(): GroupModel[] {
-    return this._parentsList;
+  public get groups(): GroupModel[] {
+    return this._groups;
   }
 
-  protected _getParentGroup() {
-    this._service.list().subscribe((list: GroupModel[]) => {
-      this._parentsList = list;
-    });
+  public get parentId() {
+    return this._parentId;
   }
 
   constructor(service: GroupService,
               router: Router,
               route: ActivatedRoute) {
     super(service, router, route);
-    this._getParentGroup();
+  }
+
+  public ngOnInit(): void {
+    this._getGroups();
+    this._query();
+  }
+
+  protected _getGroups() {
+    this._service.list().subscribe((list: GroupModel[]) => {
+      this._groups = list;
+      super.ngOnInit();
+    });
+  }
+
+  protected _query() {
+    this._route.params.subscribe(params => {
+      this._parentId = +params['parentId'];
+    });
+  }
+
+  protected _load() {
+    super._load();
+
+    if (this._parentId) {
+      this._model.parentId = this._parentId;
+    }
   }
 }
 
