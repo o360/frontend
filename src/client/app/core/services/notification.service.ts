6209b0a60f3556a53c0da06107ecec1c7ad32b64
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-20 16:14:28 +0700

    SOP-62 Notification service implementation

--- src/client/app/core/services/notification.service.ts
+++ src/client/app/core/services/notification.service.ts
@@ -4,85 +4,60 @@ import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class NotificationService {
-  public customOptions: ToastOptions = {
-    positionClass: '',
-    maxShown: 1,
-    newestOnTop: true,
-    animate: '',
-    toastLife: 1,
-    enableHTML: false,
-    dismiss: '',
-    messageClass: '',
-    titleClass: '',
-    showCloseButton: true
+  private _customOptions: ToastOptions = {
+    toastLife: 5000,
+    newestOnTop:  true,
+    maxShown: 5,
+    showCloseButton: true,
+    animate:'fade',
+    positionClass:'toast-top-right',
+    messageClass: null,
+    titleClass:'font-bold'
   };
 
-  constructor(protected _toast: ToastsManager,
+  constructor(protected _toastsManager: ToastsManager,
               protected _translate: TranslateService) {
   }
 
-  public showSuccess(message?: string, title?: string) {
-    if (title && message) {
-      this._toast.success(this._translateCustomText(message, 'T_SUCCESS_MESSAGE_'),
-        this._translateCustomText(title, 'T_SUCCESS_TITLE_'));
-    } else if (message) {
-      this._toast.success(this._translateCustomText(message, 'T_SUCCESS_MESSAGE_'),
-        this._translateDefaultText('T_SUCCESS_TITLE_DEFAULT'));
-    } else {
-      this._toast.success(this._translateDefaultText('T_SUCCESS_MESSAGE_DEFAULT'),
-        this._translateDefaultText('T_SUCCESS_TITLE_DEFAULT'));
-    }
+  public success(message?: string, title?: string) {
+    let messageText = message && message.length ? message : 'T_SUCCESS_MESSAGE_DEFAULT';
+    let titleText = title && title.length ? title : 'T_SUCCESS_TITLE_DEFAULT';
+    messageText = this._prepareMessage(messageText, 'T_SUCCESS_MESSAGE_');
+    titleText = this._prepareMessage(titleText, 'T_SUCCESS_TITLE_');
+    this._toastsManager.success(messageText, titleText, this._customOptions);
   }
 
-  public showError(message?: string, title?: string) {
-    if (title && message) {
-      this._toast.error(this._translateCustomText(message, 'T_ERROR_MESSAGE_'),
-        this._translateCustomText(title, 'T_ERROR_TITLE_'));
-    } else if (message) {
-      this._toast.error(this._translateCustomText(message, 'T_SUCCESS_MESSAGE_'),
-        this._translateDefaultText('T_ERROR_TITLE_DEFAULT'));
-    } else {
-      this._toast.error(this._translateDefaultText('T_ERROR_MESSAGE_DEFAULT'),
-        this._translateDefaultText('T_ERROR_TITLE_DEFAULT'));
-    }
+  public error(message?: string, title?: string) {
+    let messageText = message && message.length ? message : 'T_ERROR_MESSAGE_DEFAULT';
+    let titleText = title && title.length ? title : 'T_ERROR_TITLE_DEFAULT';
+    messageText = this._prepareMessage(messageText, 'T_ERROR_MESSAGE_');
+    titleText = this._prepareMessage(titleText, 'T_ERROR_TITLE_');
+    this._toastsManager.error(messageText, titleText, this._customOptions);
   }
 
-  public showWarning(message?: string, title?: string) {
-    if (title && message) {
-      this._toast.warning(this._translateCustomText(message, 'T_WARNING_MESSAGE_'),
-        this._translateCustomText(title, 'T_WARNING_TITLE_'));
-    } else if (message) {
-      this._toast.warning(this._translateCustomText(message, 'T_WARNING_MESSAGE_'),
-        this._translateDefaultText('T_WARNING_TITLE_DEFAULT'));
-    } else {
-      this._toast.warning(this._translateDefaultText('T_WARNING_MESSAGE_DEFAULT'),
-        this._translateDefaultText('T_WARNING_TITLE_DEFAULT'));
-    }
+  public warning(message?: string, title?: string) {
+    let messageText = message && message.length ? message : 'T_WARNING_MESSAGE_DEFAULT';
+    let titleText = title && title.length ? title : 'T_WARNING_TITLE_DEFAULT';
+    messageText = this._prepareMessage(messageText, 'T_WARNING_MESSAGE_');
+    titleText = this._prepareMessage(titleText, 'T_WARNING_TITLE_');
+    this._toastsManager.warning(messageText, titleText, this._customOptions);
   }
 
-  public showInfo(message?: string, title?: string) {
-    if (title && message) {
-      this._toast.info(this._translateCustomText(message, 'T_INFO_MESSAGE_'),
-        this._translateCustomText(title, 'T_INFO_TITLE_'));
-    } else if (message) {
-      this._toast.info(this._translateCustomText(message, 'T_INFO_MESSAGE_'),
-        this._translateDefaultText('T_INFO_TITLE_DEFAULT'));
-    } else {
-      this._toast.info(this._translateDefaultText('T_INFO_MESSAGE_DEFAULT'),
-        this._translateDefaultText('T_INFO_TITLE_DEFAULT'));
-    }
+  public info(message?: string, title?: string) {
+    let messageText = message && message.length ? message : 'T_INFO_MESSAGE_DEFAULT';
+    let titleText = title && title.length ? title : 'T_INFO_TITLE_DEFAULT';
+    messageText = this._prepareMessage(messageText, 'T_INFO_MESSAGE_');
+    titleText = this._prepareMessage(titleText, 'T_INFO_TITLE_');
+    this._toastsManager.info(messageText, titleText, this._customOptions);
   }
 
-  protected _translateCustomText(text: string, type: string) {
-    return this._translate.instant(type + text.toUpperCase());
-  }
-
-  protected _translateDefaultText(text: string) {
-    return this._translate.instant(text);
+  protected _prepareMessage(text: string, type?: string) {
+    let translate = type ? (type + text.toUpperCase()) : text;
+    return this._translate.instant(translate);
   }
 
   public clearAll() {
-    return this._toast.clearAllToasts();
+    return this._toastsManager.clearAllToasts();
   }
 }
 
