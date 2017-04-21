fa5a787a5fa109c46859294a10b4a1ac08041477
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-21 12:42:59 +0700

    SOP-62 Notification service implementation

--- src/client/app/core/services/notification.service.ts
+++ src/client/app/core/services/notification.service.ts
@@ -6,13 +6,13 @@ import { TranslateService } from '@ngx-translate/core';
 export class NotificationService {
   private _customOptions: ToastOptions = {
     toastLife: 5000,
-    newestOnTop:  true,
+    newestOnTop: true,
     maxShown: 5,
     showCloseButton: true,
-    animate:'fade',
-    positionClass:'toast-top-right',
+    animate: 'fade',
+    positionClass: 'toast-top-right',
     messageClass: null,
-    titleClass:'font-bold'
+    titleClass: 'font-bold'
   };
 
   constructor(protected _toastsManager: ToastsManager,
@@ -22,38 +22,29 @@ export class NotificationService {
   public success(message?: string, title?: string) {
     let messageText = message && message.length ? message : 'T_SUCCESS_MESSAGE_DEFAULT';
     let titleText = title && title.length ? title : 'T_SUCCESS_TITLE_DEFAULT';
-    messageText = this._prepareMessage(messageText, 'T_SUCCESS_MESSAGE_');
-    titleText = this._prepareMessage(titleText, 'T_SUCCESS_TITLE_');
-    this._toastsManager.success(messageText, titleText, this._customOptions);
+    this._toastsManager.success(this._prepareMessage(messageText), this._prepareMessage(titleText), this._customOptions);
   }
 
   public error(message?: string, title?: string) {
     let messageText = message && message.length ? message : 'T_ERROR_MESSAGE_DEFAULT';
     let titleText = title && title.length ? title : 'T_ERROR_TITLE_DEFAULT';
-    messageText = this._prepareMessage(messageText, 'T_ERROR_MESSAGE_');
-    titleText = this._prepareMessage(titleText, 'T_ERROR_TITLE_');
-    this._toastsManager.error(messageText, titleText, this._customOptions);
+    this._toastsManager.error(this._prepareMessage(messageText), this._prepareMessage(titleText), this._customOptions);
   }
 
   public warning(message?: string, title?: string) {
     let messageText = message && message.length ? message : 'T_WARNING_MESSAGE_DEFAULT';
     let titleText = title && title.length ? title : 'T_WARNING_TITLE_DEFAULT';
-    messageText = this._prepareMessage(messageText, 'T_WARNING_MESSAGE_');
-    titleText = this._prepareMessage(titleText, 'T_WARNING_TITLE_');
-    this._toastsManager.warning(messageText, titleText, this._customOptions);
+    this._toastsManager.warning(this._prepareMessage(messageText), this._prepareMessage(titleText), this._customOptions);
   }
 
   public info(message?: string, title?: string) {
     let messageText = message && message.length ? message : 'T_INFO_MESSAGE_DEFAULT';
     let titleText = title && title.length ? title : 'T_INFO_TITLE_DEFAULT';
-    messageText = this._prepareMessage(messageText, 'T_INFO_MESSAGE_');
-    titleText = this._prepareMessage(titleText, 'T_INFO_TITLE_');
-    this._toastsManager.info(messageText, titleText, this._customOptions);
+    this._toastsManager.info(this._prepareMessage(messageText), this._prepareMessage(titleText), this._customOptions);
   }
 
-  protected _prepareMessage(text: string, type?: string) {
-    let translate = type ? (type + text.toUpperCase()) : text;
-    return this._translate.instant(translate);
+  protected _prepareMessage(text: string) {
+    return this._translate.instant(text);
   }
 
   public clearAll() {
