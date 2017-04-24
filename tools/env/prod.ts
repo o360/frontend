64abb262cb5fb3ea59e0b1e5d5037dcaaeeada10
Sergey Pudov <pudov_sa@bw-sw.com>
2017-04-24 11:43:00 +0700

    Hotfix - Prod config added

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -1,7 +1,18 @@
 import { EnvConfig } from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
-  ENV: 'PROD'
+  ENV: 'PROD',
+  PROVIDERS: {
+    'google': {
+      authorizationUrlBase: 'https://accounts.google.com/o/oauth2/auth',
+      getParams: {
+        response_type: 'code',
+        client_id: '183984693644-rrf60igolgdvtmdq5oue0opi3jvq4vl8.apps.googleusercontent.com',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+        redirect_uri: 'http://sop-ci.z1.netpoint-dc.com/login/google'
+      }
+    }
+  }
 };
 
 export = ProdConfig;
