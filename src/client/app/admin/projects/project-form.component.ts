3020e2a3a256b40aa3622aadbd85f10f75287730
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-25 16:31:29 +0700

    SOP-76, 75 CRUD for Projects - done, SOP- 77, 78 Project relation partly (api not ready yet)

--- src/client/app/admin/projects/project-form.component.ts
+++ src/client/app/admin/projects/project-form.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { ModelId } from '../../core/models/model';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormComponent } from '../../shared/components/form.component';
 import { ProjectModel } from '../../core/models/project-model';
 import { ProjectService } from '../../core/services/project.service';
