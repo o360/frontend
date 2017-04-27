447664a1f6b86f5d9a30132be376be14b8df313a
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-04-27 13:15:19 +0700

    SOP - 71 Users in groups

--- src/client/app/admin/groups/group-user-list.component.ts
+++ src/client/app/admin/groups/group-user-list.component.ts
@@ -3,9 +3,8 @@ import { UserModel } from '../../core/models/user-model';
 import { UserService } from '../../core/services/user.service';
 import { ListComponent } from '../../shared/components/list.component';
 import { ModelId } from '../../core/models/model';
-import { IQueryParams } from '../../core/services/rest.service';
 import { GroupService } from '../../core/services/group.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -13,8 +12,7 @@ import { ActivatedRoute, Params } from '@angular/router';
   templateUrl: 'group-user-list.component.html'
 })
 export class GroupUserListComponent extends ListComponent<UserModel> implements OnChanges {
-  protected _groupId: ModelId = 'null';
-  public concreteList: boolean = false;
+  protected _groupId: string = 'null';
 
   @Input()
   public set groupId(value: string) {
@@ -27,41 +25,42 @@ export class GroupUserListComponent extends ListComponent<UserModel> implements 
 
   public ngOnChanges(changes: SimpleChanges) {
     if (changes['groupId']) {
-      this._queryParams = { groupId: this._groupId };
-      this._updateParams();
+      this._queryParams = Object.assign(this._queryParams, { groupId: this._groupId });
+      console.log(this._queryParams);
+      this._update();
     }
   }
 
   public addUser(userId: ModelId) {
     this._routeParams();
-    this._groupService.userToGroup(this._groupId, userId).subscribe(() => this._update());
+    this._groupService.addUser(this._groupId, userId).subscribe(() => this._update());
   }
 
   public delete(groupId?: ModelId, userId?: ModelId) {
-    this._groupService.userRemoveFromGroup(groupId, userId).subscribe(() => this._updateParams());
+    this._groupService.removeUser(groupId, userId).subscribe(() => this._update());
   }
 
+  public changeState(mode: string) {
+    mode === 'addMode'
+      ? delete this._queryParams.groupId
+      : this._queryParams = Object.assign(this._queryParams, { groupId: this._groupId });
+    this._update();
+  }
 
   constructor(service: UserService,
-              protected _groupService: GroupService,
-              protected _route: ActivatedRoute) {
-    super(service);
+              activatedRoute: ActivatedRoute,
+              router: Router,
+              protected _groupService: GroupService) {
+    super(service, activatedRoute, router);
   }
 
   protected _routeParams() {
-    this._route.params.forEach(this._processRouteParams.bind(this));
+    this._activatedRoute.params.forEach(this._processRouteParams.bind(this));
   }
 
   protected _processRouteParams(params: Params) {
     if (params['groupId']) {
       this._groupId = params['groupId'];
     }
-  }
-
-  protected _updateParams() {
-    let queryParams: IQueryParams = { groupId: this._groupId };
-    this._groupId = queryParams.groupId;
-    this._update(queryParams);
-    this.concreteList = true;
   }
 }
--- src/client/app/admin/groups/groups-routing.module.ts
+++ src/client/app/admin/groups/groups-routing.module.ts
@@ -16,10 +16,6 @@ import { GroupUserListComponent } from './group-user-list.component';
       component: GroupFormComponent,
       data: { breadcrumb: 'T_ACTION_CREATE' },
     }, {
-      path: 'user-list',
-      component: GroupUserListComponent,
-      data: { breadcrumb: 'T_USERS' },
-    }, {
       path: ':id',
       component: GroupDetailsComponent,
       data: { breadcrumbIgnore: true },
