9d2d06cdc30e18dd774dd3d7c2e2568c4a7077eb
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-05-22 13:05:19 +0700

    SOP-146 Problem with form cloning from the list

--- src/client/app/core/services/form.service.ts
+++ src/client/app/core/services/form.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { RestServiceConfig } from '../decorators/rest-service-config.decorator';
-import { RestService } from './rest.service';
-import { FormModel } from '../models/form-model';
-import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { RestServiceConfig } from '../decorators/rest-service-config.decorator';
+import { FormModel } from '../models/form-model';
+import { AuthService } from './auth.service';
 import { NotificationService } from './notification.service';
+import { RestService } from './rest.service';
 
 @Injectable()
 @RestServiceConfig({
