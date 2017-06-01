741e761de1c0e988489991e9d13ab3cdbbdc7e6d
qqcky <darya.baklanova@gmail.com>
2017-06-01 20:54:58 +0700

    SOP-99 Validation for form builders

--- src/client/app/admin/forms/form-builder.component.ts
+++ src/client/app/admin/forms/form-builder.component.ts
@@ -1,44 +1,154 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormElementType, FormModel, IFormElement } from '../../core/models/form-model';
+import { FormElement, FormElementType, FormModel } from '../../core/models/form-model';
 import { FormService } from '../../core/services/form.service';
 import { NotificationService } from '../../core/services/notification.service';
 import { FormComponent } from '../../shared/components/form.component';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+export const RequireValue = (kind: string): boolean => {
+  return [
+    FormElementType.Radio,
+    FormElementType.Checkboxgroup,
+    FormElementType.Select
+  ].includes(kind);
+};
+
+export const FormBuilderValidator = (control: AbstractControl): ValidationErrors => {
+  const elements = (control.get('elements') as FormArray).value;
+  return elements.length ? null : {noElements: true};
+};
+
+export const FormElementValidator = (control: AbstractControl): ValidationErrors => {
+  const values = (control.get('values') as FormArray).value;
+  return (!!values.length) ? null : {noValues: 'T_FORM_VALUES_ARE_REQUIRED'};
+}
 
 @Component({
   moduleId: module.id,
   selector: 'bs-form-builder',
   templateUrl: 'form-builder.component.html'
 })
-export class FormBuilderComponent extends FormComponent<FormModel> {
+export class FormBuilderComponent extends FormComponent<FormModel> implements OnInit {
+  protected static _idSeq = 0;
+
   protected _returnPath = ['/admin/forms'];
   protected _elementTypes: string[] = Object.values(FormElementType);
+  protected _form: FormGroup;
+
+  protected _index: number = FormBuilderComponent.next();
+
+  protected static next() {
+    return FormBuilderComponent._idSeq++;
+  }
+
+  public get index(): number {
+    return this._index;
+  }
 
   public get elementTypes(): string[] {
     return this._elementTypes;
   }
 
+  public get form(): FormGroup {
+    return this._form;
+  }
+
+  public get elements(): FormArray {
+    return this.form.get('elements') as FormArray;
+  }
+
+  public requireValue(kind: string) {
+    return RequireValue(kind);
+  }
+
   constructor(service: FormService,
               router: Router,
               route: ActivatedRoute,
-              notificationService: NotificationService) {
+              notificationService: NotificationService,
+              protected _formBuilder: FormBuilder) {
     super(service, router, route, notificationService);
+
+    this._createForm();
+  }
+
+  protected _processModel(model: FormModel) {
+    super._processModel(model);
+
+    this._form.reset({
+      name: this._model.name
+    });
+    this._setElements(this._model.elements);
+  }
+
+  public save() {
+    this._model = this._prepareSaveForm();
+
+    super.save();
   }
 
   public addElement(kind: string) {
-    let element: IFormElement = {
+    let element: FormElement = new FormElement({
       kind: kind,
       caption: null,
       required: false,
-      values: []
-    };
-    this._model.elements.push(element);
+    });
+
+    if (RequireValue(kind)) {
+      element.values = [];
+    }
+
+    this.elements.push(this._prepareFormGroup(element));
   }
 
-  public deleteElement(element: IFormElement) {
-    let index: number = this._model.elements.indexOf(element);
-    if (index !== -1) {
-      this._model.elements.splice(index, 1);
+  public deleteElement(index: number) {
+    this.elements.removeAt(index);
+  }
+
+  protected _createForm() {
+    this._form = this._formBuilder.group({
+        name: ['', Validators.required],
+        elements: this._formBuilder.array([])
+      },
+      {validator: FormBuilderValidator});
+  }
+
+  protected _setElements(elements: FormElement[]) {
+    let elementFGs = elements.map(element => this._prepareFormGroup(element));
+    let elementFormArray = this._formBuilder.array(elementFGs);
+    this._form.setControl('elements', elementFormArray);
+  }
+
+  protected _prepareSaveForm(): FormModel {
+    let formModel = this._form.value;
+    let elementsCopy: FormElement[] = formModel.elements.map((element: FormElement) => Object.assign({}, element));
+
+    let saveForm: FormModel = new FormModel({
+      name: formModel.name,
+      elements: elementsCopy
+    });
+    return saveForm;
+  }
+
+  protected _prepareFormGroup(element: FormElement) {
+    let form: FormGroup;
+
+    if (RequireValue(element.kind)) {
+      form = this._formBuilder.group({
+          caption: [element.caption, Validators.required],
+          required: element.required,
+          valueCaption: '',
+          values: this._formBuilder.array(element.values),
+          kind: element.kind
+        },
+        {validator: FormElementValidator});
+    } else {
+      form = this._formBuilder.group({
+        caption: [element.caption, Validators.required],
+        required: element.required,
+        kind: element.kind
+      });
     }
+    return form;
   }
 }
