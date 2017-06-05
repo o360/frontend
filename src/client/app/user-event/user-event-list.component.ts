40f8819d0e4a2f680704ea894629290e51602221
qqcky <darya.baklanova@gmail.com>
2017-06-05 23:27:06 +0700

    SOP-167 Assessment forms by types (WIP)

--- src/client/app/user-event/user-event-list.component.ts
+++ src/client/app/user-event/user-event-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ListComponent } from '../shared/components/list.component';
-import { EventModel } from '../core/models/event-model';
+import { EventModel, EventStatus } from '../core/models/event-model';
 import { EventService } from '../core/services/event.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NotificationService } from '../core/services/notification.service';
@@ -19,6 +19,10 @@ export class UserEventListComponent extends ListComponent<EventModel> {
     this._status = value;
   }
 
+  public get EventStatus() {
+    return EventStatus;
+  }
+
   constructor(service: EventService,
               activatedRoute: ActivatedRoute,
               router: Router,
@@ -27,7 +31,7 @@ export class UserEventListComponent extends ListComponent<EventModel> {
   }
 
   protected _update() {
-    let queryParams = { status: this._status, sort: 'start', onlyAvailable: 'true' };
+    let queryParams = {status: this._status, sort: 'start', onlyAvailable: 'true'};
 
     this._service.list(queryParams).subscribe((res: IListResponse<EventModel>) => {
       this._meta = res.meta;
