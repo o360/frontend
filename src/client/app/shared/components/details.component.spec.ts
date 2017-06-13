47108e405de37feeb968028e36ca9f28df7a986e
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-06-13 09:43:08 +0700

    SOP-131 Stubs into a separated file

--- src/client/app/shared/components/details.component.spec.ts
+++ src/client/app/shared/components/details.component.spec.ts
@@ -1,73 +1,54 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component, OnInit } from '@angular/core';
-import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { TestModel } from '../../core/models/model.spec';
-import { IListResponse, IQueryParams, RestService } from '../../core/services/rest.service';
-import { DetailsComponent } from './details.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpModule, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 import { TestService } from '../../core/services/rest.service.spec';
-import { Subject } from 'rxjs/Subject';
-
-export class ActivatedRouteStub {
-  // public params: Subject<Params> = new Subject<Params>();
-  protected _testParams: IQueryParams;
-  public params: Observable<Params>;
-  public parent = {
-    params: Observable.of({})
-  };
-
-  get testParams() {
-    return this._testParams;
-  }
-
-  set testParams(params: IQueryParams) {
-    this._testParams = params;
-  }
-}
-
-export class RestServiceStub {
-  public get() {
-    return new Observable<TestModel>();
-  }
-}
-
-export class RouterStub {
-}
+import { TestModel } from '../../core/models/model.spec';
+import { NotificationService } from '../../core/services/notification.service';
+import { AuthService } from '../../core/services/auth.service';
+import { DetailsComponent } from './details.component';
+import { ActivatedRouteStub, AuthServiceStub, NotificationServiceStub, RouterStub } from '../../stubs/stubs.utils';
 
 @Component({
   moduleId: module.id,
-  selector: 'bs-test-details',
+  selector: 'bs-test-list',
   template: ''
 })
 export class TestDetailsComponent extends DetailsComponent<TestModel> implements OnInit {
-  protected _testResponse: IListResponse<TestModel> = {
-    data: [new TestModel({ 'id': 1, 'name': 'test' }), new TestModel({ 'id': 2, 'name': 'test1' })],
-    meta: {
-      total: 2,
-      size: 2,
-      number: 1
-    }
-  };
+  protected _testModel: TestModel = new TestModel({id:1, name:'test1'});
 
-  constructor(service: RestService<TestModel>, route: ActivatedRoute) {
-    super(service, route);
+  constructor(service: TestService,
+              activatedRoute: ActivatedRoute) {
+    super(service, activatedRoute);
+  }
+
+  public ngOnInit() {
+    this._update();
+  }
+
+  protected _update() {
+    super._update();
+    this._model = this._testModel;
   }
 }
 
 export function main() {
-  describe('DetailsComponent', () => {
-    let testService: RestServiceStub;
-    let activatedRoute: ActivatedRouteStub;
+  describe('Details Component', () => {
     let comp: TestDetailsComponent;
     let fixture: ComponentFixture<TestDetailsComponent>;
-    let testModel: TestModel;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
+        imports: [HttpModule],
         declarations: [TestDetailsComponent],
-        providers: [ TestService,
-          { provide: RestService, useClass: RestServiceStub },
-          { provide: ActivatedRoute, useClass: ActivatedRouteStub, useValue: { 'params': Observable.from([{ 'id': 1 }]) } }
+        providers: [
+          TestService,
+          { provide: NotificationService, useClass: NotificationServiceStub },
+          { provide: AuthService, useClass: AuthServiceStub },
+          { provide: XHRBackend, useClass: MockBackend },
+          { provide: ActivatedRoute, useClass: ActivatedRouteStub },
+          {provide: Router, useClass: RouterStub}
         ]
       });
 
@@ -75,76 +56,18 @@ export function main() {
 
       comp = fixture.componentInstance;
       comp.ngOnInit();
-
-      // testService = <any>fixture.debugElement.injector.get(RestService);
-      // activatedRoute = <any>fixture.debugElement.injector.get(ActivatedRoute);
     });
 
-    afterEach(() => {
-      testService = undefined;
-      activatedRoute = undefined;
-    });
-
-    it('should create a component and define a model', () => {
-      expect(comp).toBeDefined();
+    it('should define a model', () => {
       expect(comp.model).toBeDefined();
+      expect(comp.model instanceof TestModel).toBeTruthy();
     });
 
-    it('should create a component and define a model', () => {
-      expect(comp).toBeDefined();
-      expect(comp.model).toBeDefined();
+    it('should call update', () => {
+      const testUpdate = spyOn(comp, '_update');
+
+      comp.ngOnInit();
+      expect(testUpdate).toHaveBeenCalledTimes(1);
     });
-
-    // it('should create a `FormGroup` comprised of `FormControl`s', () => {
-    //   fixture.detectChanges();
-    //   params
-    //   // spyOn(testService, 'get').and.returnValue(Observable.of(testModel));
-    //   // expect(testService.get()).toHaveBeenCalled();
-    //   // comp.ngOnInit();
-    //   // expect(comp.model instanceof TestModel).toBe(true);
-    // });
-
-
-    // it('', () => {
-    //   // let mockParams = new ActivatedRouteStub({'id': '1'});
-    //   inject([RestServiceStub], (testService: RestServiceStub) => {
-    //     testService.get().subscribe(res => {
-    //       expect(res.name).toBe('test');
-    //     });
-    //   });
-    //   // expect(comp.model);
-    //   // expect(testService.get()).toHaveBeenCalled();
-    // });
-
-    // it('should resolve test data', () => {
-    //   const spy = spyOn(testService, 'get').and.returnValue(
-    //     Observable.of(TestModel)
-    //   );
-    //   comp.ngOnInit();
-    //   fixture.detectChanges();
-    //   expect(comp.ngOnInit().spyOn)
-    //   // expect(comp.model).toEqual(TestModel);
-    //   // expect(spy.calls.any()).toEqual(true);
-    // });
-
-    // it('should resolve test data', () => {
-    //   const spy = spyOn(testService, 'get').and.returnValue(
-    //     Observable.of(TestModel)
-    //   );
-    //   comp.ngOnInit();
-    //   fixture.detectChanges();
-    //   expect(comp.model).toEqual(TestModel);
-    //   expect(spy.calls.any()).toEqual(true);
-    // });
-    //
-    // it('should display 1st hero\'s name', () => {
-    //   // const expectedHero = testModel;
-    //   activatedRoute.params = n;
-    //     // { id: testModel.id };
-    //   createComponent().then(() => {
-    //     expect(page.nameDisplay.textContent).toBe(expectedHero.name);
-    //   });
-    // });
   });
 }
-
