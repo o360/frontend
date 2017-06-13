beffddc87ba2929d5a0f6c31bd62625706c0abc4
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-06-13 09:43:08 +0700

    SOP-131 Stubs into a separated file

---
+++ src/client/app/stubs/stubs.utils.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { IQueryParams, RestService } from '../core/services/rest.service';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { RestServiceConfig } from '../core/decorators/rest-service-config.decorator';
+import { TestModel } from '../core/models/model.spec';
+
+/* Notification service stub */
+export class NotificationServiceStub {
+  public success() {
+    return;
+  }
+
+  public error() {
+    return;
+  }
+
+  public warning() {
+    return;
+  }
+
+  public info() {
+    return;
+  }
+
+  public clearAll() {
+    return;
+  }
+}
+
+/* Auth service stub */
+export class AuthServiceStub {
+  public get isLoggedIn() {
+    return true;
+  }
+
+  public get token() {
+    return 'test-token';
+  }
+}
+
+/* Router stub */
+export class RouterStub {
+  public navigate() {
+    return Promise.resolve(true);
+  }
+}
+
+/* ActivatedRouteStub stub */
+@Injectable()
+export class ActivatedRouteStub {
+  protected _testParams: IQueryParams = { number: '2' };
+  protected _subject = new BehaviorSubject(this.testParams);
+  public params = this._subject.asObservable();
+
+  get testParams() {
+    return this._testParams;
+  }
+
+  set testParams(params: IQueryParams) {
+    this._testParams = params;
+    this._subject.next(params);
+  }
+}
