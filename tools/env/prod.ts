805a57289a37fbcf4aa4bd5ae93376633ff37b73
qqcky <darya.baklanova@gmail.com>
2017-06-21 17:47:32 +0700

    SOP-250 preparing for production

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -10,7 +10,7 @@ const ProdConfig: EnvConfig = {
         response_type: 'code',
         client_id: '183984693644-rrf60igolgdvtmdq5oue0opi3jvq4vl8.apps.googleusercontent.com',
         scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
-        redirect_uri: 'http://sop-ci.z1.netpoint-dc.com/login/google'
+        redirect_uri: 'http://sop.bw-sw.com/login/google'
       }
     }
   }
