dd67bc30a7d22dada78e410fbed73adc384bb82d
qqcky <darya.baklanova@gmail.com>
2017-06-22 17:26:07 +0700

    SOP-254 TimeZone by default

--- src/client/app/shared/config/timezone.config.ts
+++ src/client/app/shared/config/timezone.config.ts
@@ -7,7 +7,6 @@ export class TimeZone {
   public static readonly UTC_6: string = '-06:00';
   public static readonly UTC_5: string = '-05:00';
   public static readonly UTC_4: string = '-04:00';
-  public static readonly UTC_3_30: string = '-03:30';
   public static readonly UTC_3: string = '-03:00';
   public static readonly UTC_2: string = '-02:00';
   public static readonly UTC_1: string = '-01:00';
@@ -15,25 +14,15 @@ export class TimeZone {
   public static readonly UTC1: string = '+01:00';
   public static readonly UTC2: string = '+02:00';
   public static readonly UTC3: string = '+03:00';
-  public static readonly UTC3_30: string = '+03:30';
   public static readonly UTC4: string = '+04:00';
-  public static readonly UTC4_30: string = '+04:30';
   public static readonly UTC5: string = '+05:00';
-  public static readonly UTC5_30: string = '+05:30';
-  public static readonly UTC5_45: string = '+05:45';
   public static readonly UTC6: string = '+06:00';
-  public static readonly UTC6_30: string = '+06:30';
   public static readonly UTC7: string = '+07:00';
   public static readonly UTC8: string = '+08:00';
-  public static readonly UTC8_30: string = '+08:30';
-  public static readonly UTC8_45: string = '+08:45';
   public static readonly UTC9: string = '+09:00';
-  public static readonly UTC9_30: string = '+09:30';
   public static readonly UTC10: string = '+10:00';
-  public static readonly UTC10_30: string = '+10:30';
   public static readonly UTC11: string = '+11:00';
   public static readonly UTC12: string = '+12:00';
-  public static readonly UTC12_45: string = '+12:45';
   public static readonly UTC13: string = '+13:00';
   public static readonly UTC14: string = '+14:00';
 }
