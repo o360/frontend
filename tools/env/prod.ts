8d430f83d51a22b1ae82a541d82558a36bb000e1
qqcky <darya.baklanova@gmail.com>
2017-06-23 11:55:25 +0700

    SOP-250 OAuth config updated

--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -2,13 +2,13 @@ import { EnvConfig } from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
   ENV: 'PROD',
-  API: 'http://sop-ci.z1.netpoint-dc.com:9000/api/v1.0',
+  API: 'https://sop.bw-sw.com/api',
   PROVIDERS: {
     'google': {
       authorizationUrlBase: 'https://accounts.google.com/o/oauth2/auth',
       getParams: {
         response_type: 'code',
-        client_id: '183984693644-rrf60igolgdvtmdq5oue0opi3jvq4vl8.apps.googleusercontent.com',
+        client_id: '127600832093-e6ka9ie7hvmp6ji8k1t3mp1l2uk9vgpl.apps.googleusercontent.com',
         scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
         redirect_uri: 'http://sop.bw-sw.com/login/google'
       }
