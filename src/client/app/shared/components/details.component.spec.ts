d9357114009c311573fe743648e4c25223d29727
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-07-13 09:26:59 +0700

    SOP-275 Fixes

--- src/client/app/shared/components/details.component.spec.ts
+++ src/client/app/shared/components/details.component.spec.ts
@@ -8,7 +8,10 @@ import { TestModel } from '../../core/models/model.spec';
 import { NotificationService } from '../../core/services/notification.service';
 import { AuthService } from '../../core/services/auth.service';
 import { DetailsComponent } from './details.component';
-import { ActivatedRouteStub, AuthServiceStub, ConfirmationStub, NotificationServiceStub, RouterStub } from '../../stubs/stubs.utils';
+import {
+  ActivatedRouteStub, AuthServiceStub, BreadcrumbServiceStub, ConfirmationStub, NotificationServiceStub,
+  RouterStub
+} from '../../stubs/stubs.utils';
 import { ConfirmationService } from '../../core/services/confirmation.service';
 import { BreadcrumbService } from '../../core/services/breadcrumb.service';
 
@@ -52,7 +55,8 @@ export function main() {
           { provide: XHRBackend, useClass: MockBackend },
           { provide: ActivatedRoute, useClass: ActivatedRouteStub },
           { provide: Router, useClass: RouterStub },
-          { provide: ConfirmationService, useClass: ConfirmationStub }
+          { provide: ConfirmationService, useClass: ConfirmationStub },
+          { provide: BreadcrumbService, useClass: BreadcrumbServiceStub },
         ]
       });
 
