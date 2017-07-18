3270c1a1e4de67c1a8608b5d8e87e1df71d7765f
Darya Baklanova <darya.baklanova@gmail.com>
2017-07-18 17:11:31 +0700

    SOP-272 Notification Service unit testing

--- src/client/app/core/services/notification.service.spec.ts
+++ src/client/app/core/services/notification.service.spec.ts
@@ -1,55 +1,27 @@
-import { ComponentFactoryResolver, Injectable, Injector } from '@angular/core';
+import { Injector } from '@angular/core';
 import { getTestBed, TestBed } from '@angular/core/testing';
-import { HttpModule, Response, ResponseOptions, ResponseType, XHRBackend } from '@angular/http';
+import { HttpModule, XHRBackend } from '@angular/http';
 import { MockBackend, MockConnection } from '@angular/http/testing';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { RestServiceConfig } from '../decorators/rest-service-config.decorator';
-import { TestModel } from '../models/model.spec';
-import { AuthService } from './auth.service';
 import { NotificationService } from './notification.service';
-import { RestService } from './rest.service';
-import { Model } from '../models/model';
-import { ConfirmationService } from './confirmation.service';
-import {
-  AuthServiceStub, ConfirmationStub, NotificationServiceStub, RouterStub, ToastsManagerStub,
-  TranslateServiceStub
-} from '../../stubs/stubs.utils';
+import { ToastsManagerStub, TranslateServiceStub } from '../../stubs/stubs.utils';
 import { TranslateService } from '@ngx-translate/core';
-import { ToastsManager } from 'ng2-toastr';
-
-export interface IListResponse<TestModel extends Model> {
-  data: TestModel[];
-}
-
-// /* Test Service */
-// @Injectable()
-// @RestServiceConfig({
-//   endpoint: '/api/',
-//   entityName: 'test',
-//   entityConstructor: TestModel
-// })
-// export class TestService extends RestService<TestModel> {
-// }
+import { ToastOptions, ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 export function main() {
-  describe('NotificationService Service', () => {
+  describe('Notification Service', () => {
     let testService: NotificationService;
+    let toastsManager: ToastsManager;
     let injector: Injector;
     let mockBackend: MockBackend;
     let connection: MockConnection;
-    let model: TestModel;
-
-    beforeEach(() => {
-      model = new TestModel();
-    });
 
     beforeEach(() => {
       TestBed.configureTestingModule({
         imports: [HttpModule],
         providers: [
           NotificationService,
-          // { provide: ToastsManager, useClass: ToastsManagerStub },
+          ToastOptions,
+          { provide: ToastsManager, useClass: ToastsManagerStub },
           { provide: TranslateService, useClass: TranslateServiceStub },
           { provide: XHRBackend, useClass: MockBackend }
         ]
@@ -57,6 +29,7 @@ export function main() {
       injector = getTestBed();
       mockBackend = <any>injector.get(XHRBackend);
       testService = injector.get(NotificationService);
+      toastsManager = injector.get(ToastsManager);
       mockBackend.connections.subscribe((c: MockConnection) => connection = c);
     });
 
@@ -73,10 +46,32 @@ export function main() {
       expect(testService instanceof NotificationService).toBeTruthy();
     });
 
-    // it('should return an Subject when loadComponent() called', () => {
-    //   let getResponse = testService.loadComponent();
-    //
-    //   expect(getResponse).toEqual(jasmine.any(Subject));
-    // });
+    it('should support 4 types of notifications', () => {
+      // success
+      const testSuccess = spyOn(toastsManager, 'success');
+      testService.success('success');
+      expect(testSuccess).toHaveBeenCalled();
+
+      // warning
+      const testWarning = spyOn(toastsManager, 'warning');
+      testService.warning('warning');
+      expect(testWarning).toHaveBeenCalled();
+
+      // info
+      const testInfo = spyOn(toastsManager, 'info');
+      testService.info('info');
+      expect(testInfo).toHaveBeenCalled();
+
+      // error
+      const testError = spyOn(toastsManager, 'error');
+      testService.error('error');
+      expect(testError).toHaveBeenCalled();
+    });
+
+    it('should clear all toasts', () => {
+      const testClear = spyOn(toastsManager, 'clearAllToasts');
+      testService.clearAll();
+      expect(testClear).toHaveBeenCalled();
+    });
   });
 }
