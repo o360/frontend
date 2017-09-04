f5657052d4b6739186a1103f390725571666f5bd
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-09-04 10:56:50 +0700

    SOP-380 Inform user If it is not possible to change the answers in assess event

--- src/client/app/user-event/assessment-project-list.component.ts
+++ src/client/app/user-event/assessment-project-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { EventStatus } from '../core/models/event-model';
 import { ListComponent } from '../shared/components/list.component';
 import { ProjectModel } from '../core/models/project-model';
 import { ModelId } from '../core/models/model';
 import { ProjectService } from '../core/services/project.service';
 import { NotificationService } from '../core/services/notification.service';
+import { EventService } from '../core/services/event.service';
 
 
 @Component({
@@ -14,15 +16,26 @@ import { NotificationService } from '../core/services/notification.service';
 })
 export class AssessmentProjectListComponent extends ListComponent<ProjectModel> implements OnInit {
   protected _eventId: ModelId;
+  protected _eventStatus: EventStatus;
 
   public get eventId(): ModelId {
     return this._eventId;
   }
 
+  public get eventStatus(): EventStatus {
+    return this._eventStatus;
+  }
+
+  public get EventStatus() {
+    return EventStatus;
+  }
+
+
   constructor(service: ProjectService,
               activatedRoute: ActivatedRoute,
               router: Router,
-              notificationService: NotificationService) {
+              notificationService: NotificationService,
+              protected _eventService: EventService) {
     super(service, activatedRoute, router, notificationService);
   }
 
@@ -32,6 +45,8 @@ export class AssessmentProjectListComponent extends ListComponent<ProjectModel> 
       this._queryParams.eventId = this._eventId.toString();
       this._update();
     });
+
+    this._getStatus();
   }
 
   public answerChanged() {
@@ -41,5 +56,11 @@ export class AssessmentProjectListComponent extends ListComponent<ProjectModel> 
   protected _update() {
     Object.assign(this._queryParams, { onlyAvailable: 'true' });
     super._update();
+  }
+
+  protected _getStatus() {
+    this._eventService.get(this._eventId).subscribe(model =>
+      this._eventStatus = model.status
+    );
   }
 }
