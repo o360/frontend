30b39b9fa08759f4a31ed9e93465182902786226
Darya Baklanova <darya.baklanova@gmail.com>
2017-09-25 12:53:23 +0700

    SOP-453 Fix tests and rebase

--- src/client/app/core/models/event-model.spec.ts
+++ src/client/app/core/models/event-model.spec.ts
@@ -1,4 +1,4 @@
-import { EventModel } from './event-model';
+import { EventModel, EventState } from './event-model';
 
 export function main() {
   describe('EventModel class', () => {
@@ -18,6 +18,22 @@ export function main() {
       model.start = '2017-06-23T11:03:45';
       model.end = '2017-06-23T11:03:45';
       expect(JSON.stringify(model.toJson())).toEqual('{"notifications":[],"start":"2017-06-23T11:03:45","end":"2017-06-23T11:03:45"}');
+    });
+
+    it('should have define state of assessment made by user', () => {
+      model.start = '2017-06-23T11:03:45';
+      model.end = '2017-06-23T11:03:45';
+      model.userInfo = {
+        totalFormsCount: 2,
+        answeredFormsCount: 0
+      };
+      expect(model.state).toEqual(EventState.NotStarted);
+
+      model.userInfo.answeredFormsCount = 1;
+      expect(model.state).toEqual(EventState.PartiallyFilled);
+
+      model.userInfo.answeredFormsCount = 2;
+      expect(model.state).toEqual(EventState.FullFilled);
     });
 
     it('should have a properties by default', () => {
