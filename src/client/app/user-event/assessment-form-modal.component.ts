46902600dff15b27d29e4171284bcfb974a5b5f9
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2017-09-25 12:41:36 +0700

    SOP-402 Add header to modal

--- src/client/app/user-event/assessment-form-modal.component.ts
+++ src/client/app/user-event/assessment-form-modal.component.ts
@@ -12,6 +12,7 @@ export class AssessmentFormModalComponent {
   protected _modal: ModalDirective;
   protected _commentItem: IComment = { formElementId: undefined, text: '' };
   protected _onCommentAdded: EventEmitter<IComment> = new EventEmitter<IComment>();
+  protected _currentComment: string;
 
   @ViewChild('commentModal')
   public set modal(value: ModalDirective) {
@@ -31,7 +32,12 @@ export class AssessmentFormModalComponent {
     return this._commentItem;
   }
 
+  public get currentComment(): string {
+    return this._currentComment;
+  }
+
   public show(item: FormElement) {
+    this._currentComment = item.tempComment ? item.tempComment : '';
     this._commentItem = { formElementId: item.id, text: item.tempComment ? item.tempComment : '' };
     this._modal.show();
   }
