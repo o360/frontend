b4f8640be1005ac97d61914188887a82088137d4
nam_vv <nam_vv@bw-sw.com>
2019-04-11 13:40:52 +0700

    Merge branch 'feature-501' of ssh://git.bw-sw.com:2222/open360/frontend into bug-501
    # Conflicts:
    #	.gitignore
    #	.gitlab-ci.yml
    #	README.md
    #	package.json
    #	test-config.js
    #	tsconfig.json
    #	tslint.json
    #	yarn.lock

--- src/client/app/core/components/footer/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'bs-footer',
-  templateUrl: 'footer.component.html'
-})
-export class FooterComponent {
-}
--- src/client/app/core/components/layout/layout.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'bs-layout',
-  templateUrl: 'layout.component.html'
-})
-export class LayoutComponent {
-}
