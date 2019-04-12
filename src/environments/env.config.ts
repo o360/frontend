afe6c96e6747c32fee5ece3c252c4b0b48951686
nam_vv <nam_vv@bw-sw.com>
2019-04-12 20:59:40 +0700

    Butch of fixes after review

--- src/environments/env.config.ts
+++ src/environments/env.config.ts
@@ -1,7 +1,40 @@
 import { EnvConfig } from './env-config.interface';
-import { isDevMode } from '@angular/core';
-import { ProdConfig } from './environment.prod';
-import { DevConfig } from './environment';
+import { environment } from './environment';
 
-export const Config: EnvConfig = isDevMode() ? DevConfig : ProdConfig;
+export const ProdConfig: EnvConfig = {
+  ENV: 'PROD',
+  API: '<API_ENDPOINT>',
+  TITLE_MAIN: 'Open360',
+  TITLE_NAV: 'Open360',
+  PROVIDERS: {
+    'google': {
+      authorizationUrlBase: 'https://accounts.google.com/o/oauth2/auth',
+      getParams: {
+        response_type: 'code',
+        client_id: '<CLIENT_ID>',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+        redirect_uri: '<REDIRECT_URI>'
+      }
+    }
+  }
+};
 
+export const DevConfig: EnvConfig = {
+  ENV: 'DEV',
+  API: '<API_ENDPOINT>',
+  TITLE_MAIN: 'Open360',
+  TITLE_NAV: 'Open360',
+  PROVIDERS: {
+    'google': {
+      authorizationUrlBase: 'https://accounts.google.com/o/oauth2/auth',
+      getParams: {
+        response_type: 'code',
+        client_id: '<CLIENT_ID>',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+        redirect_uri: '<REDIRECT_URI>'
+      }
+    }
+  }
+};
+
+export const Config: EnvConfig = environment.production ? ProdConfig : DevConfig;
