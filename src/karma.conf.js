090247b91191486b28265bc7b876435ae155c748
nam_vv <nam_vv@bw-sw.com>
2019-04-17 11:08:37 +0700

    Merge branches 'bug-501' and 'feature-504' of ssh://git.bw-sw.com:2222/open360/frontend into feature-504
    # Conflicts:
    #	src/app/core/services/rest.service.ts

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true,
   });
 };
