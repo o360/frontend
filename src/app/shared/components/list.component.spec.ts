0956f67129645d5e74b0a6e51d9b45bf41b9b930
nam_vv <nam_vv@bw-sw.com>
2019-04-22 12:03:08 +0700

    Assessment event: tooltip placement fixes

--- src/app/shared/components/list.component.spec.ts
+++ src/app/shared/components/list.component.spec.ts
@@ -6,7 +6,7 @@ import { TestService } from '../../core/services/rest.service.spec';
 import { TestModel } from '../../core/models/model.spec';
 import { NotificationService } from '../../core/services/notification.service';
 import { AuthService } from '../../core/services/auth.service';
-import { IQueryParams } from '../../core/services/rest.service';
+import { IListResponse, IQueryParams } from '../../core/services/rest.service';
 import { FilterType } from '../../core/models/filter';
 import {
   ActivatedRouteStub,
@@ -17,7 +17,56 @@ import {
   RouterStub
 } from '../../stubs/stubs.utils';
 import { ConfirmationService } from '../../core/services/confirmation.service';
-import { TestListComponent } from './test-list.component';
+import { Component, OnInit } from '@angular/core';
+import { ListComponent } from './list.component';
+import { ModelId } from '../../core/models/model';
+
+@Component({
+  selector: 'bs-test-list',
+  template: ``
+})
+export class TestListComponent extends ListComponent<TestModel> implements OnInit {
+  protected _testResponse: IListResponse<TestModel> = {
+    data: [new TestModel({ 'id': 1, 'name': 'test' }), new TestModel({ 'id': 2, 'name': 'test2' })],
+    meta: {
+      total: 2,
+      size: 2,
+      number: 1
+    }
+  };
+
+  constructor(service: TestService,
+              activatedRoute: ActivatedRoute,
+              router: Router,
+              notificationService: NotificationService) {
+    super(service, activatedRoute, router, notificationService);
+  }
+
+  public ngOnInit() {
+    this._update();
+  }
+
+  public processRequestParams() {
+    return this._processRequestParams(this._activatedRoute.params);
+  }
+
+  public delete(id: ModelId) {
+    super.delete(id);
+
+    let toDelete = this._list.filter(x => x.id === id)[0];
+    let index = this._list.indexOf(toDelete);
+    if (index !== -1) {
+      this._list.splice(index, 1);
+    }
+  }
+
+  protected _update() {
+    super._update();
+
+    this._list = this._testResponse.data;
+    this._meta = this._testResponse.meta;
+  }
+}
 
 describe('List Component', () => {
   let comp: TestListComponent;
