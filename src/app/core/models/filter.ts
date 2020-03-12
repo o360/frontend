3d25f1d2513c7e0617f3c52ff7863dc1d2ef1210
vanekbr <br.ivan.sg@gmail.com>
2020-03-12 15:22:19 +0700

    27 Bugfix anonymity reset on form clear (#78)

--- src/app/core/models/filter.ts
+++ src/app/core/models/filter.ts
@@ -12,16 +12,16 @@
  * limitations under the License.
  */
 
-export class FilterType {
-  public static readonly String: string = 'string';
-  public static readonly Number: string = 'number';
-  public static readonly Select: string = 'select';
+export enum FilterType {
+  STRING = 'STRING',
+  NUMBER = 'NUMBER',
+  SELECT = 'SELECT',
 }
 
 export class Filter {
   public name: string;
   public field: string;
-  public type: string;
+  public type: FilterType;
   public values?: any[];
   public value?: any;
 }
